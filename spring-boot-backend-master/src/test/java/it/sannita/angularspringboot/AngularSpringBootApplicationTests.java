dce8f46d1ce31ad3362cbd2a410de406c6f0335e
silviasav <74418348+silviasav@users.noreply.github.com>
2020-11-13 16:07:33 +0100

    Create AngularSpringBootApplicationTests.java

---
+++ spring-boot-backend-master/src/test/java/it/sannita/angularspringboot/AngularSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sannita.angularspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
