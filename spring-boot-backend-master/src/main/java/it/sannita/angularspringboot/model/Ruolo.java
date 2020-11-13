c2c25ff6e8c53cf8d972ef347b1b83c39053f206
silviasav <74418348+silviasav@users.noreply.github.com>
2020-11-13 16:06:38 +0100

    Create Ruolo.java

---
+++ spring-boot-backend-master/src/main/java/it/sannita/angularspringboot/model/Ruolo.java
@@ -0,0 +1,109 @@
+package it.sannita.angularspringboot.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.Date;
+
+
+/**
+ * The persistent class for the ruolo database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Ruolo.findAll", query="SELECT r FROM Ruolo r")
+public class Ruolo implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private int id;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="data_fine")
+	private Date dataFine;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="data_inizio")
+	private Date dataInizio;
+
+	@Column(name="id_ruolo")
+	private int idRuolo;
+
+	private String password;
+
+	private String profilo;
+
+	private String ruolo;
+
+	//bi-directional many-to-one association to Dipendente
+	@ManyToOne
+	@JoinColumn(name="id_dipendente")
+	private Dipendente dipendente;
+
+	public Ruolo() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDataFine() {
+		return this.dataFine;
+	}
+
+	public void setDataFine(Date dataFine) {
+		this.dataFine = dataFine;
+	}
+
+	public Date getDataInizio() {
+		return this.dataInizio;
+	}
+
+	public void setDataInizio(Date dataInizio) {
+		this.dataInizio = dataInizio;
+	}
+
+	public int getIdRuolo() {
+		return this.idRuolo;
+	}
+
+	public void setIdRuolo(int idRuolo) {
+		this.idRuolo = idRuolo;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getProfilo() {
+		return this.profilo;
+	}
+
+	public void setProfilo(String profilo) {
+		this.profilo = profilo;
+	}
+
+	public String getRuolo() {
+		return this.ruolo;
+	}
+
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}
+
+	public Dipendente getDipendente() {
+		return this.dipendente;
+	}
+
+	public void setDipendente(Dipendente dipendente) {
+		this.dipendente = dipendente;
+	}
+
+}
