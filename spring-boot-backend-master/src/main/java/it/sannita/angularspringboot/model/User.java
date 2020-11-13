780e2bb0506a67e7e79fb015f957d99b0975b752
silviasav <74418348+silviasav@users.noreply.github.com>
2020-11-13 16:07:06 +0100

    Create User.java

---
+++ spring-boot-backend-master/src/main/java/it/sannita/angularspringboot/model/User.java
@@ -0,0 +1,50 @@
+package it.sannita.angularspringboot.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the user database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+
+	private String email;
+
+	private String name;
+
+	public User() {
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
